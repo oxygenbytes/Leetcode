af0db5be40d794ab095851622ffc17759a3f3680
Zhang Xueqing <oxygenbytes@gmail.com>
2022-12-25 00:13:09 +0800

    20221225

---
+++ Cplus/leetcode/editor/cn/[剑指 Offer 68 - II]二叉树的最近公共祖先.cpp
@@ -0,0 +1,72 @@
+//给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。 
+//
+// 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（
+//一个节点也可以是它自己的祖先）。” 
+//
+// 例如，给定如下二叉树: root = [3,5,1,6,2,0,8,null,null,7,4] 
+//
+// 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+//输出: 3
+//解释: 节点 5 和节点 1 的最近公共祖先是节点 3。
+// 
+//
+// 示例 2: 
+//
+// 输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+//输出: 5
+//解释: 节点 5 和节点 4 的最近公共祖先是节点 5。因为根据定义最近公共祖先节点可以为节点本身。
+// 
+//
+// 
+//
+// 说明: 
+//
+// 
+// 所有节点的值都是唯一的。 
+// p、q 为不同节点且均存在于给定的二叉树中。 
+// 
+//
+// 注意：本题与主站 236 题相同：https://leetcode-cn.com/problems/lowest-common-ancestor-of-
+//a-binary-tree/ 
+//
+// Related Topics 树 深度优先搜索 二叉树 👍 504 👎 0
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left, *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root) return nullptr;
+        if (root == p || root == q) return root;
+        // 如果 p 和 q 都存在，则返回它们的公共祖先；
+        // 如果只存在一个，则返回存在的一个；
+        // 如果 p 和 q 都不存在，则返回NULL
+        TreeNode *l = lowestCommonAncestor(root->left, p, q);
+        TreeNode *r = lowestCommonAncestor(root->right, p, q);
+        if (l && r) return root;
+        return l ? l : r;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
