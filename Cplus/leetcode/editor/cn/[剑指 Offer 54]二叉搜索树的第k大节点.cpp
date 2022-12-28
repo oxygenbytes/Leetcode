1486358ff5c33d21cf1cf2c10a99481f4c67c901
Zhang Xueqing <oxygenbytes@gmail.com>
2022-12-28 22:30:38 +0800

    20221228

---
+++ Cplus/leetcode/editor/cn/[剑指 Offer 54]二叉搜索树的第k大节点.cpp
@@ -0,0 +1,74 @@
+//给定一棵二叉搜索树，请找出其中第 k 大的节点的值。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: root = [3,1,4,null,2], k = 1
+//   3
+//  / \
+// 1   4
+//  \
+//   2
+//输出: 4 
+//
+// 示例 2: 
+//
+// 
+//输入: root = [5,3,6,2,4,null,null,1], k = 3
+//       5
+//      / \
+//     3   6
+//    / \
+//   2   4
+//  /
+// 1
+//输出: 4 
+//
+// 
+//
+// 限制： 
+//
+// 
+// 1 ≤ k ≤ 二叉搜索树元素个数 
+// 
+//
+// Related Topics 树 深度优先搜索 二叉搜索树 二叉树 👍 349 👎 0
+
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left, *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> ans;
+    int kthLargest(TreeNode* root, int k) {
+        dfs(root);
+        reverse(ans.begin(), ans.end());
+        return ans[k-1];
+    }
+
+    void dfs(TreeNode *root) {
+        if (!root) return ;
+        dfs(root->left);
+        ans.push_back(root->val);
+        dfs(root->right);
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
