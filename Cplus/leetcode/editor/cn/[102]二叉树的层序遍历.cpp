5535889289e393f4207284ef2fdb526c95b21019
Zhang Xueqing <oxygenbytes@gmail.com>
2023-02-17 23:58:13 +0800

    20230217

---
+++ Cplus/leetcode/editor/cn/[102]二叉树的层序遍历.cpp
@@ -0,0 +1,90 @@
+// 102 二叉树的层序遍历
+
+//给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：root = [3,9,20,null,null,15,7]
+//输出：[[3],[9,20],[15,7]]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [1]
+//输出：[[1]]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目在范围 [0, 2000] 内 
+// -1000 <= Node.val <= 1000 
+// 
+//
+// Related Topics 树 广度优先搜索 二叉树 👍 1574 👎 0
+
+#include <vector>
+#include <queue>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        if (!root) return {};
+
+        vector<vector<int>> res;
+        vector<int> cur;
+        queue<TreeNode*> q({root});
+
+        while(q.size()) {
+
+            cur.clear();
+
+            for (int n = q.size() - 1;n >= 0;n--) {
+                auto temp = q.front(); q.pop();
+                cur.push_back(temp->val);
+                if (temp->left) q.push(temp->left);
+                if (temp->right) q.push(temp->right);                
+            }
+            res.push_back(cur);
+        }
+        return res;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
