a9efc8f7718fbfa2471c99b449e5b56478a6d663
Zhang Xueqing <oxygenbytes@gmail.com>
2023-02-19 21:43:58 +0800

    20230219

---
+++ Cplus/leetcode/editor/cn/[543]二叉树的直径.cpp
@@ -0,0 +1,74 @@
+// 543 二叉树的直径
+
+//给定一棵二叉树，你需要计算它的直径长度。一棵二叉树的直径长度是任意两个结点路径长度中的最大值。这条路径可能穿过也可能不穿过根结点。 
+//
+// 
+//
+// 示例 : 给定二叉树 
+//
+//           1
+//         / \
+//        2   3
+//       / \     
+//      4   5    
+// 
+//
+// 返回 3, 它的长度是路径 [4,2,1,3] 或者 [5,2,1,3]。 
+//
+// 
+//
+// 注意：两结点之间的路径长度是以它们之间边的数目表示。 
+//
+// Related Topics 树 深度优先搜索 二叉树 👍 1240 👎 0
+
+#include <algorithm>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int ans = 0;
+public:
+    int diameterOfBinaryTree(TreeNode *root) {
+        if (!root) return 0;
+        dfs(root);
+        return ans;
+    }
+
+    int dfs(TreeNode *root) {
+        if (!root) return 0;
+        int l = dfs(root->left);
+        int r = dfs(root->right);
+
+        // 两结点之间的路径长度是以它们之间边的数目表示。
+        ans = max(ans, l + r);
+        return max(l, r) + 1;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
