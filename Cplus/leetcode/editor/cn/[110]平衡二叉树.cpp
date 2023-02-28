947c058a4c001d9e4a653abf629c5f943eb01c08
Zhang Xueqing <oxygenbytes@gmail.com>
2023-02-28 21:27:23 +0800

    20230228

---
+++ Cplus/leetcode/editor/cn/[110]平衡二叉树.cpp
@@ -0,0 +1,85 @@
+// 110 平衡二叉树
+
+//给定一个二叉树，判断它是否是高度平衡的二叉树。 
+//
+// 本题中，一棵高度平衡二叉树定义为： 
+//
+// 
+// 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1 。 
+// 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：root = [3,9,20,null,null,15,7]
+//输出：true
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：root = [1,2,2,3,3,null,null,4,4]
+//输出：false
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = []
+//输出：true
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中的节点数在范围 [0, 5000] 内 
+// -10⁴ <= Node.val <= 10⁴ 
+// 
+//
+// Related Topics 树 深度优先搜索 二叉树 👍 1251 👎 0
+
+#include <algorithm>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if (!root) return true;
+        if (!root->left && !root->right) return true;
+        return isBalanced(root->left) && isBalanced(root->right)
+            && abs(height(root->left) - height(root->right)) <= 1;
+    }
+
+    int height(TreeNode *root) {
+        if (!root) return 0;
+
+        return max(height(root->left), height(root->right)) + 1;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
