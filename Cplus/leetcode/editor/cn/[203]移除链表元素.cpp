741faaa5d2f32a304198144288b9633de8733f0f
Zhang Xueqing <oxygenbytes@gmail.com>
2023-03-03 23:13:53 +0800

    20230303

---
+++ Cplus/leetcode/editor/cn/[203]移除链表元素.cpp
@@ -0,0 +1,83 @@
+// 203 移除链表元素
+
+//给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：head = [1,2,6,3,4,5,6], val = 6
+//输出：[1,2,3,4,5]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [], val = 1
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = [7,7,7,7], val = 7
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 列表中的节点数目在范围 [0, 10⁴] 内 
+// 1 <= Node.val <= 50 
+// 0 <= val <= 50 
+// 
+//
+// Related Topics 递归 链表 👍 1174 👎 0
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        auto dummy = new ListNode(-1);
+        dummy->next = head;
+
+        auto ptr = dummy;
+
+        while(ptr->next) {
+            if (ptr->next->val == val) {
+                ListNode *temp = ptr->next;
+                ptr->next = ptr->next->next;
+                delete(temp);
+            } else {
+                ptr = ptr->next;
+            }
+        }
+
+        head = dummy->next;
+        delete(dummy);
+        return head;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
